2a9978dcdd44c279c4898e8d5d41c8bd21eed95a
Oscar Carrasco <anubis.carrasco03@gmail.com>
2026-01-19 12:59:00 -0600

    chore(db): call BootTaxSeeder from DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,14 +9,16 @@ use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
+    //use WithoutModelEvents;
 
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            BootTaxSeeder::class,
+        ]);
 
         User::factory()->create([
             'first_name' => 'Test',
