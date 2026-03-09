000dd7b5b09ee6a55c3caf676a6b2165276d2c41
Oscar Carrasco <anubis.carrasco03@gmail.com>
2026-03-09 12:43:13 -0600

    chore(container): register ScheduledTaskRepository binding

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 use App\Data\Repositories\CompanyRepository;
+use App\Data\Repositories\ScheduledTaskRepository;
 use App\Data\Repositories\UserRepository;
 use App\Domain\Interfaces\Repositories\CompanyRepositoryInterface;
+use App\Domain\Interfaces\Repositories\ScheduledTaskRepositoryInterface;
 use App\Domain\Interfaces\Repositories\UserRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
@@ -17,7 +19,8 @@ class RepositoryServiceProvider extends ServiceProvider
     {
         $bindings = [
             CompanyRepositoryInterface::class => CompanyRepository::class,
-            UserRepositoryInterface::class => UserRepository::class
+            ScheduledTaskRepositoryInterface::class => ScheduledTaskRepository::class,
+            UserRepositoryInterface::class => UserRepository::class,
         ];
 
         foreach ($bindings as $abstract => $concrete) {
