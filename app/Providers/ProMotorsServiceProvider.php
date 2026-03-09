a0593797b67e77c4cb1b046f82a71d0a4a5224b0
Oscar Carrasco <anubis.carrasco03@gmail.com>
2026-03-09 12:43:27 -0600

    chore(container): register ScheduledTaskService binding

--- app/Providers/ProMotorsServiceProvider.php
+++ app/Providers/ProMotorsServiceProvider.php
@@ -8,6 +8,8 @@ use App\Application\Services\Auth\TokenService;
 use App\Application\Services\Auth\TokenServiceImpl;
 use App\Application\Services\Company\CompanyService;
 use App\Application\Services\Company\CompanyServiceImpl;
+use App\Application\Services\ScheduledTask\ScheduledTaskService;
+use App\Application\Services\ScheduledTask\ScheduledTaskServiceImpl;
 use App\Application\Services\Subscriptions\SubscriptionService;
 use App\Application\Services\Subscriptions\SubscriptionServiceImpl;
 use App\Application\Services\User\UserService;
@@ -24,15 +26,18 @@ class ProMotorsServiceProvider extends ServiceProvider
         $bindings = [
             // Auth services
             TokenService::class => TokenServiceImpl::class,
-            AuthService::class  => AuthServiceImpl::class,
+            AuthService::class => AuthServiceImpl::class,
 
             // Catalogs services
-            //ProductService::class => ProductServiceImpl::class,
+            // ProductService::class => ProductServiceImpl::class,
 
             // Settings services
             CompanyService::class => CompanyServiceImpl::class,
             UserService::class => UserServiceImpl::class,
             SubscriptionService::class => SubscriptionServiceImpl::class,
+
+            // Scheduler services
+            ScheduledTaskService::class => ScheduledTaskServiceImpl::class,
         ];
 
         foreach ($bindings as $abstract => $concrete) {
